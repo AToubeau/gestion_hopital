853e769bc290f35bd6debf350f2f9cf23d7fd84f
antoine <antoine.toubeau@gmail.com>
2025-02-20 12:39:56 +0100

    uml first draw

---
+++ src/test/java/com/example/spring/demo/gestion_hopital/GestionHopitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.demo.gestion_hopital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionHopitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
