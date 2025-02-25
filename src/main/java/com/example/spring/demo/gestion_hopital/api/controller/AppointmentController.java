fa51bcb271803ae0f247beb036395e0f6c28c547
antoine <antoine.toubeau@gmail.com>
2025-02-25 14:07:14 +0100

    return ResponseEntity and use validator

--- src/main/java/com/example/spring/demo/gestion_hopital/api/controller/AppointmentController.java
+++ src/main/java/com/example/spring/demo/gestion_hopital/api/controller/AppointmentController.java
@@ -6,6 +6,7 @@ import com.example.spring.demo.gestion_hopital.bll.exception.ressourceNotFound.R
 import com.example.spring.demo.gestion_hopital.bll.service.AppointmentService;
 import com.example.spring.demo.gestion_hopital.dal.domain.entity.Appointment;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,29 +18,40 @@ public class AppointmentController {
     private final AppointmentService appointmentService;
 
     @GetMapping("/all")
-    public List<AppointmentDto> getAllAppointments() {
-        return appointmentService.getAllAppointments()
-                .stream()
-                .map(AppointmentDto::fromEntity)
-                .toList();
+    public ResponseEntity<List<AppointmentDto>> getAllAppointments() {
+        return ResponseEntity.ok(
+                appointmentService.getAllAppointments()
+                        .stream()
+                        .map(AppointmentDto::fromEntity)
+                        .toList()
+        );
     }
 
     @GetMapping("/{id}")
-    public Appointment getAppointmentById(@PathVariable Long id) {
-        return appointmentService.getAppointmentById(id).orElseThrow(() -> new RessourceNotFoundException("Appointment not found"));
+    public ResponseEntity<AppointmentDto> getAppointmentById(@PathVariable Long id) {
+        return ResponseEntity.ok(
+                AppointmentDto.fromEntity(
+                        appointmentService.getAppointmentById(id)
+                                .orElseThrow(() -> new RessourceNotFoundException("Appointment not found"))
+                )
+        );
     }
 
     @PutMapping("/{id}")
-    public AppointmentDto updateAppointment(@PathVariable Long id, @RequestBody AppointmentForm appointmentForm) {
-        return AppointmentDto.fromEntity(
-                appointmentService.updateAppointment(id, AppointmentForm.toEntity(appointmentForm))
+    public ResponseEntity<AppointmentDto> updateAppointment(@PathVariable Long id, @RequestBody AppointmentForm appointmentForm) {
+        return ResponseEntity.ok(
+                AppointmentDto.fromEntity(
+                        appointmentService.updateAppointment(id, AppointmentForm.toEntity(appointmentForm))
+                )
         );
     }
 
     @PostMapping("/create")
-    public AppointmentDto createAppointment(@RequestBody AppointmentForm appointmentForm) {
-        return AppointmentDto.fromEntity(
-                appointmentService.createAppointment(appointmentForm.startDateTime())
+    public ResponseEntity<AppointmentDto> createAppointment(@RequestBody AppointmentForm appointmentForm) {
+        return ResponseEntity.ok(
+                AppointmentDto.fromEntity(
+                        appointmentService.createAppointment(appointmentForm.startDateTime())
+                )
         );
     }
 
--- src/main/java/com/example/spring/demo/gestion_hopital/api/form/AppointmentForm.java
+++ src/main/java/com/example/spring/demo/gestion_hopital/api/form/AppointmentForm.java
@@ -1,10 +1,14 @@
 package com.example.spring.demo.gestion_hopital.api.form;
 
 import com.example.spring.demo.gestion_hopital.dal.domain.entity.Appointment;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
 public record AppointmentForm(
+        @DateTimeFormat
+        @NotNull
         LocalDateTime startDateTime
 ) {
     public static Appointment toEntity(AppointmentForm appointmentForm) {
